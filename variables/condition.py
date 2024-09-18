1007345c4878878959c1f8f4670292d757123eb5
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-09-18 22:20:47 +0530

    conditions

---
+++ variables/condition.py
@@ -0,0 +1,8 @@
+person = "Divya"
+
+if len(person) > 5:
+    print("Its a female")
+elif len(person) == 5:
+    print("Trans" ) 
+else:
+    print("Its a male")
---
+++ variables/data_structure.py
@@ -0,0 +1,10 @@
+# List (array)
+my_list = ["sam", "ram"]
+
+print(my_list[1])
+
+my_list.append("jam")
+my_list.remove("jam")
+
+
+print(my_list)
