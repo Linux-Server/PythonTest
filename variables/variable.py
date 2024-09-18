ef7059bcfc5064980bef23ff71b1cd81e50a107a
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-09-18 22:09:27 +0530

    variables

---
+++ variables/variable.py
@@ -0,0 +1,18 @@
+# Variables : to store data
+# {name} = {value}
+
+name = "Sachin" 
+print(type(name))
+print(len(name))
+
+num = 10
+print(num)
+print(type(num))
+
+flt = 10.992342425
+print(type(flt))
+print((flt))
+
+
+# Data type : intger -int, float 32/64, boolean, string-str
+
