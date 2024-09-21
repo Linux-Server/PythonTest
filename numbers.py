e977d7e7f809f066ced09436deace270ad42410d
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-09-21 09:27:38 +0530

    numbers

---
+++ numbers.py
@@ -0,0 +1,19 @@
+from math import floor
+print((3 + 5) * 3)
+
+my_num = "22"
+print(type(int(my_num)))
+
+my_num = -6
+
+print(abs(my_num))
+
+print(pow(3,2))
+
+print(max(4,5))
+
+print(min(20,5))
+
+print(round(10.63))
+
+print(floor(3.7))
