34ca30fe32b311a6dacd4783005ccbd3676fc83f
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-09-23 10:26:59 +0530

    list

---
+++ inputs.py
@@ -0,0 +1,9 @@
+# name = input("Enter ur namne?")
+
+# print("Name of the user is " + name)
+
+num_one = input("Enter the number?")
+num_two = input("Enter another number?")
+
+print(float(num_one) + float(num_two))
+
---
+++ lists.py
@@ -0,0 +1,14 @@
+friends = ["Kevin", "Kiran", "Sam"]
+print(friends)
+print(friends[0])
+# to acces last element in list
+print(friends[-1])
+
+# to access list element using range operatoe
+
+print(friends[0:3])
+print(friends[1:])
+
+# modify a list
+friends[0] ="Sachin"
+print(friends)
